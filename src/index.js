e8c1f4d57bdfcda430660d3423fb68b2b7b980b6
mayurkode7 <mayurkode7@gmail.com>
2024-02-24 03:45:17 +0530

    prettier

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // import App from './App';
-import Root from './routes/root';
-import About from './pages/About';
-import Standard from './pages/Standard';
-import Percentage from './pages/Percentage';
+import Root from "./routes/root";
+import About from "./pages/About";
+import Standard from "./pages/Standard";
+import Percentage from "./pages/Percentage";
 
-const root = createRoot(document.getElementById('app'));
+const root = createRoot(document.getElementById("app"));
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Standard />,
   },
   {
-    path: '/about',
+    path: "/about",
     element: <About />,
   },
   {
-    path: '/standard',
+    path: "/standard",
     element: <Standard />,
   },
   {
-    path: '/percentage',
+    path: "/percentage",
     element: <Percentage />,
   },
 ]);
 
 root.render(
   <StrictMode>
-
-    <RouterProvider router={router}  />
-    
+    <RouterProvider router={router} />
   </StrictMode>
 );
--- src/pages/Percentage.js
+++ src/pages/Percentage.js
@@ -1,90 +1,103 @@
-import React, { useState } from 'react';
-import '../style.css';
+import React, { useState } from 'react'
+import '../style.css'
 import Header from '../components/Header'
-import Alert from 'react-bootstrap/Alert';
-
-import Stack from 'react-bootstrap/Stack';
-
-import Container from 'react-bootstrap/Container';
-
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
-import { evaluate } from 'mathjs';
+import Alert from 'react-bootstrap/Alert'
+import Stack from 'react-bootstrap/Stack'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Row from 'react-bootstrap/Row'
+import { evaluate } from 'mathjs'
 
 export default function Percentage() {
-  const [state, setState] = useState({
-    from: '',
-    to: '',
-    fromToResult: null,
-  });
-  const keypadClick = (e) => {
-    let input = e.target.innerHTML;
-    let _operation = state.operation + input;
-    setState({ ...state, operation: _operation });
-  };
+    const [state, setState] = useState({
+        from: '',
+        to: '',
+        fromToResult: null,
+    })
+    const keypadClick = (e) => {
+        let input = e.target.innerHTML
+        let _operation = state.operation + input
+        setState({ ...state, operation: _operation })
+    }
 
-  const calculate = () => { };
+    const calculate = () => {}
 
-  const clear = () => { };
+    const clear = () => {}
 
-  const clearLastEntry = () => { };
+    const clearLastEntry = () => {}
 
-  const showTransactions = () => {
-    alert('feature coming soon...');
-  };
+    const showTransactions = () => {
+        alert('feature coming soon...')
+    }
 
-  const calculateFromTo = () => {
-    const from = state.from;
-    const to = state.to;
-    let r1 = evaluate(`${to} - ${from}`)
-    let r2 = evaluate(`${r1}/${from}`);
-    let r3 = evaluate(`${r2} * 100`)
-    setState({ ...state, fromToResult: "" + r3 })
-  }
+    const calculateFromTo = () => {
+        const from = state.from
+        const to = state.to
+        let r1 = evaluate(`${to} - ${from}`)
+        let r2 = evaluate(`${r1}/${from}`)
+        let r3 = evaluate(`${r2} * 100`)
+        setState({ ...state, fromToResult: '' + r3 })
+    }
 
-  return (
-    <div style={{maxWidth: '450px', margin: '0 auto'}}>
-      <Header title="Percentage" showTransactions={showTransactions} />
+    return (
+        <div style={{ maxWidth: '450px', margin: '0 auto' }}>
+            <Header title="Percentage" showTransactions={showTransactions} />
 
-      <Container fluid="sm">
-        <Form>
-          <Row className="align-items-center">
-            <p>What is the percentage increase/decrease</p>
-            <Col xs="5">
+            <Container fluid="sm">
+                <Form>
+                    <Row className="align-items-center">
+                        <p>What is the percentage increase/decrease</p>
+                        <Col xs="5">
+                            <InputGroup className="mb-1">
+                                <InputGroup.Text>From</InputGroup.Text>
+                                <Form.Control
+                                    name="from"
+                                    onChange={(e) => {
+                                        setState({
+                                            ...state,
+                                            from: e.target.value,
+                                            fromToResult: null,
+                                        })
+                                    }}
+                                    id="inlineFormInputGroup"
+                                    placeholder="for e.g 25"
+                                />
+                            </InputGroup>
+                        </Col>
+                        <Col xs="5">
+                            <InputGroup className="mb-1">
+                                <InputGroup.Text>To</InputGroup.Text>
+                                <Form.Control
+                                    onChange={(e) => {
+                                        setState({
+                                            ...state,
+                                            to: e.target.value,
+                                            fromToResult: null,
+                                        })
+                                    }}
+                                    id="inlineFormInputGroup"
+                                    placeholder="for e.g 100"
+                                />
+                            </InputGroup>
+                        </Col>
 
-              <InputGroup className="mb-1">
-                <InputGroup.Text>From</InputGroup.Text>
-                <Form.Control
-                  name="from"
-                  onChange={(e) => {  setState({ ...state, from: e.target.value, fromToResult: null }) }}
-                  id="inlineFormInputGroup"
-                  placeholder="for e.g 25" />
-              </InputGroup>
-            </Col>
-            <Col xs="5">
-
-              <InputGroup className="mb-1">
-                <InputGroup.Text>To</InputGroup.Text>
-                <Form.Control
-                  onChange={(e) => { setState({ ...state, to: e.target.value, fromToResult: null  }) }}
-                  id="inlineFormInputGroup" placeholder="for e.g 100" />
-              </InputGroup>
-            </Col>
-
-            <Col xs="2">
-              <Button className="mb-2" onClick={calculateFromTo}>
-                Calculate
-              </Button>
-            </Col>
-          </Row>
-        </Form>
-        {state.fromToResult !== null && <Alert variant="info">
-          Percentage change from {state.from} to {state.to} is {state.fromToResult} %
-        </Alert>}
-      </Container>
-    </div>
-  );
+                        <Col xs="2">
+                            <Button className="mb-2" onClick={calculateFromTo}>
+                                Calculate
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
+                {state.fromToResult !== null && (
+                    <Alert variant="info">
+                        Percentage change from {state.from} to {state.to} is{' '}
+                        {state.fromToResult} %
+                    </Alert>
+                )}
+            </Container>
+        </div>
+    )
 }
