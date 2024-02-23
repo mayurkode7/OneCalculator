e91ecef02c1d267f0372744aef0b84e85516360c
mayurkode7 <mayurkode7@gmail.com>
2024-02-24 03:16:23 +0530

    percentage plus minus is done

--- src/pages/Percentage.js
+++ src/pages/Percentage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import '../style.css';
 import Header from '../components/Header'
-
+import Alert from 'react-bootstrap/Alert';
 
 import Stack from 'react-bootstrap/Stack';
 
@@ -12,13 +12,13 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
-
+import { evaluate } from 'mathjs';
 
 export default function Percentage() {
   const [state, setState] = useState({
     from: '',
     to: '',
-    fromToResult: '',
+    fromToResult: null,
   });
   const keypadClick = (e) => {
     let input = e.target.innerHTML;
@@ -36,36 +36,54 @@ export default function Percentage() {
     alert('feature coming soon...');
   };
 
+  const calculateFromTo = () => {
+    const from = state.from;
+    const to = state.to;
+    let r1 = evaluate(`${to} - ${from}`)
+    let r2 = evaluate(`${r1}/${from}`);
+    let r3 = evaluate(`${r2} * 100`)
+    setState({ ...state, fromToResult: "" + r3 })
+  }
+
   return (
     <>
       <Header title="Percentage" showTransactions={showTransactions} />
 
       <Container fluid="sm">
-      <Form>
-      <Row className="align-items-center">
-        <p>What is the percentage increase/decrease</p>
-        <Col xs="5">
-         
-          <InputGroup className="mb-1">
-            <InputGroup.Text>From</InputGroup.Text>
-            <Form.Control id="inlineFormInputGroup" placeholder="for e.g 25" />
-          </InputGroup>
-        </Col>
-        <Col xs="5">
-        
-          <InputGroup className="mb-1">
-            <InputGroup.Text>To</InputGroup.Text>
-            <Form.Control id="inlineFormInputGroup" placeholder="for e.g 100" />
-          </InputGroup>
-        </Col>
-        
-        <Col xs="2">
-          <Button type="submit" className="mb-2">
-            Submit
-          </Button>
-        </Col>
-      </Row>
-    </Form>
+        <Form>
+          <Row className="align-items-center">
+            <p>What is the percentage increase/decrease</p>
+            <Col xs="5">
+
+              <InputGroup className="mb-1">
+                <InputGroup.Text>From</InputGroup.Text>
+                <Form.Control
+                  name="from"
+                  onChange={(e) => {  setState({ ...state, from: e.target.value, fromToResult: null }) }}
+                  id="inlineFormInputGroup"
+                  placeholder="for e.g 25" />
+              </InputGroup>
+            </Col>
+            <Col xs="5">
+
+              <InputGroup className="mb-1">
+                <InputGroup.Text>To</InputGroup.Text>
+                <Form.Control
+                  onChange={(e) => { setState({ ...state, to: e.target.value, fromToResult: null  }) }}
+                  id="inlineFormInputGroup" placeholder="for e.g 100" />
+              </InputGroup>
+            </Col>
+
+            <Col xs="2">
+              <Button className="mb-2" onClick={calculateFromTo}>
+                Calculate
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+        {state.fromToResult !== null && <Alert variant="info">
+          Percentage change from {state.from} to {state.to} is {state.fromToResult} %
+        </Alert>}
       </Container>
     </>
   );
