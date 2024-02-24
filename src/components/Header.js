8ddb7ddea94956b3f5ffa83e735d3b65fc96fd00
mayurkode7 <mayurkode7@gmail.com>
2024-02-24 17:45:46 +0530

    theme button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,56 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Menus from '../menus';
-import { compute } from '../utils';
-import { Calculator, House, List, ClockHistory } from 'react-bootstrap-icons';
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Menus from '../menus'
+import { compute } from '../utils'
+import {
+    Calculator,
+    House,
+    List,
+    ClockHistory,
+    MoonStars,
+} from 'react-bootstrap-icons'
 
-function Header({title, showTransactions}) {
-  return (
-    <Container fluid="sm">
-        <Row>
-          <Col xs={9}>
-            <h4>{title}</h4>
-          </Col>
-          <Col xs={3}>
-            <Button
-              variant="outline-secondary"
-              size="sm"
-              className="mr-2"
-              style={{ marginRight: 10 }}
-              onClick={showTransactions}
-            >
-              <i>
-                <ClockHistory size={20} />
-              </i>
-            </Button>
-            <Menus />
-          </Col>
-        </Row>
-        <hr />
-      </Container>
-  )
+function Header({ title, showTransactions }) {
+    return (
+        <Container fluid="sm">
+            <Row>
+                <Col>
+                    <h4>{title}</h4>
+                </Col>
+                <Col xs={5}>
+                    <Button
+                        variant="outline-secondary"
+                        size="sm"
+                        className="mr-2"
+                        style={{ marginRight: 10 }}
+                        onClick={showTransactions}
+                    >
+                        <i>
+                            <ClockHistory size={20} />
+                        </i>
+                    </Button>
+                    <Button
+                        variant="outline-secondary"
+                        size="sm"
+                        className="mr-2"
+                        style={{ marginRight: 10 }}
+                        onClick={() => {
+                            alert('Night theme coming soon')
+                        }}
+                    >
+                        <i>
+                            <MoonStars size={20} />
+                        </i>
+                    </Button>
+                    <Menus />
+                </Col>
+            </Row>
+            <hr />
+        </Container>
+    )
 }
 
 export default Header
